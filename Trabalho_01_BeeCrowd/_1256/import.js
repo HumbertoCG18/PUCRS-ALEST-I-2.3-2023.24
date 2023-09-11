555c8633a4614ff28afcfe49477d8ab068bd5159
Humberto Corrêa <humberto.cgomes17@gmail.com>
2023-09-11 12:43:43 -0300

    Delete import.js

--- Trabalho_01_BeeCrowd/_1256/import.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import * as ast from '@readme/markdown';
-import rdmd from '@readme/markdown';
-
-export default ({ body }) => (
-  <div className="markdown-body">
-    {rdmd(body)}
-  </div>
-);
